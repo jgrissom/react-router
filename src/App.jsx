c2442d219710f5b52d4b9897df04a1c94be5cb5b
jgrissom <jgrissom91@gmail.com>
2024-09-26 15:47:32 -0500

    [1.07] Utilize NavLink's isActive property 🚀 🐧 Conditionally render bold font on the active link

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import About from "./components/About";
 import { Routes, Route, NavLink } from "react-router-dom";
 
 function App() {
+  const style = ({ isActive }) => ({
+    fontWeight: isActive ? "bold" : "normal",
+  });
   return (
     <div>
       <h1>React Router</h1>
       <nav>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink to="/home" style={style}>
+          Home
+        </NavLink>
+        <NavLink to="/products" style={style}>
+          Products
+        </NavLink>
+        <NavLink to="/about" style={style}>
+          About
+        </NavLink>
       </nav>
       <div className="content">
         <Routes>
